dfe11b7b883d740af9c7b6f7e95544704e6a4340
apinanyogaratnam <apinanapinan@icloud.com>
2022-05-17 00:28:07 -0400

    refactor: fetching pokemon data

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use yew::prelude::*;
+use reqwest;
+use serde_json::Value;
 
 fn main() {
     yew::start_app::<Root>();
@@ -8,6 +10,20 @@ fn main() {
 fn root() -> Html {
     let onclick = Callback::from(|mouse_event: MouseEvent| {
         web_sys::console::log_1(&mouse_event);
+
+        wasm_bindgen_futures::spawn_local(async move {
+            let response = reqwest::get("https://pokeapi.co/api/v2/pokemon/22")
+                .await
+                .unwrap();
+
+            let text = response.text().await.unwrap();
+
+            let v: Value = serde_json::from_str(&text).unwrap();
+            let name = v["name"].as_str().unwrap();
+            let image_src = v["sprites"]["front_default"].as_str().unwrap();
+
+            web_sys::console::log_2(&name.into(), &image_src.into());
+        });
     });
 
     html! {

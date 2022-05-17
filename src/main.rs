6cc6f7dde5dc8331b2a56ad19ecb1628914bf60f
apinanyogaratnam <apinanapinan@icloud.com>
2022-05-17 12:20:29 -0400

    refactor: state handling with fetched data

--- src/main.rs
+++ src/main.rs
@@ -17,8 +17,10 @@ struct Pokemon {
 fn root() -> Html {
     let pokemon_state = use_state_eq::<Option<Pokemon>, _>(|| None);
 
-    let onclick = Callback::from(|mouse_event: MouseEvent| {
+    let onclick = Callback::from(move |mouse_event: MouseEvent| {
         web_sys::console::log_1(&mouse_event);
+
+        let pokemon_state = pokemon_state.clone();
 
         wasm_bindgen_futures::spawn_local(async move {
             let response = reqwest::get("https://pokeapi.co/api/v2/pokemon/22")
@@ -30,6 +32,13 @@ fn root() -> Html {
             let v: Value = serde_json::from_str(&text).unwrap();
             let name = v["name"].as_str().unwrap();
             let image_src = v["sprites"]["front_default"].as_str().unwrap();
+
+            let pokemon = Pokemon {
+                id: 22,
+                name: name.to_string(),
+                image_src: image_src.to_string(),
+            };
+            pokemon_state.set(Some(pokemon));
 
             web_sys::console::log_2(&name.into(), &image_src.into());
         });

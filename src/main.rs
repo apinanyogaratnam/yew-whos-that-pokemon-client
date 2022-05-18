4c973e474e3c052b3251ceb3d836627fa0939d72
apinanyogaratnam <apinanapinan@icloud.com>
2022-05-17 22:22:40 -0400

    refactor: resetting state on pokemon change

--- src/main.rs
+++ src/main.rs
@@ -18,12 +18,17 @@ struct Pokemon {
 #[function_component(Root)]
 fn root() -> Html {
     let pokemon_state = use_state_eq::<Option<Pokemon>, _>(|| None);
+    let guess_state = use_state_eq::<Option<Guess>, _>(|| None);
+    let guess_state_outer = guess_state.clone();
+
     web_sys::console::log_1(&format!("{:?}", pokemon_state).into());
     let pokemon_state_outer = pokemon_state.clone();
 
     let onclick = Callback::from(move |mouse_event: MouseEvent| {
         web_sys::console::log_1(&mouse_event);
 
+        pokemon_state.set(None);
+        guess_state.set(None);
         let pokemon_state = pokemon_state.clone();
 
         wasm_bindgen_futures::spawn_local(async move {
@@ -55,7 +60,7 @@ fn root() -> Html {
     html! {
         <div>
             <button {onclick}>{"get pokemon"}</button>
-            <ViewPokemon pokemon={(*pokemon_state_outer).clone()} />
+            <ViewPokemon pokemon={(*pokemon_state_outer).clone()} guess_state={guess_state_outer.clone()} />
         </div>
     }
 }
@@ -63,6 +68,7 @@ fn root() -> Html {
 #[derive(Properties, PartialEq)]
 struct ViewPokemonProps {
     pokemon: Option<Pokemon>,
+    guess_state: UseStateHandle<Option<Guess>>,
 }
 
 #[derive(PartialEq, Debug, Clone)]
@@ -78,8 +84,8 @@ fn view_pokemon(props: &ViewPokemonProps) -> Html {
         None => return html!{},
     };
 
-    let guess_state = use_state_eq::<Option<Guess>, _>(|| None);
-    let guess_state_outer = guess_state.clone();
+    let guess_state_outer = props.guess_state.clone();
+    let guess_state = props.guess_state.clone();
 
     let name = pokemon.name.clone();
     let input_ref = NodeRef::default();
